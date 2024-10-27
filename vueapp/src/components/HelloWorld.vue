958736be776fffb06000dacd0bd0ffc374f34ab1
Ly Lam <llam@fairventureslab.ca>
2024-10-27 14:30:08 -0400

    switch to vuetify

---
+++ vueapp/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout class="rounded rounded-md h-100">
+    <v-app-bar title="Hello">
+      <template v-slot:append>
+        <v-btn
+          icon="mdi-cog"
+          v-tooltip="'Club administration'"
+        ></v-btn>
+        <v-btn
+          icon="mdi-play"
+          v-tooltip="'Return to playing screen'"
+        ></v-btn>
+        <v-btn
+          icon="mdi-theme-light-dark"
+          @click="toggleTheme"
+          v-tooltip="isDarkTheme ? 'Switch to light theme' : 'Switch to dark theme'"
+        ></v-btn>
+      </template>
+    </v-app-bar>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+import { useTheme } from "vuetify";
+let isDarkTheme = true;
+const theme = useTheme();
+
+function toggleTheme() {
+  isDarkTheme = !isDarkTheme;
+  theme.global.name.value = isDarkTheme ? "dark" : "light";
+}
+</script>
+
